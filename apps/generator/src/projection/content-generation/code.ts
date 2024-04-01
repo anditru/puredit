22ca92d5d064055f2e613e9edc8a4d1fcc7a870c
Andre Trump <anditru@web.de>
2024-04-01 18:29:54 +0200

    Make class

--- apps/generator/src/projection/content-generation/code.ts
+++ apps/generator/src/projection/content-generation/code.ts
@@ -11,115 +11,130 @@ export function scanCode(samples: Tree[], language: Language, ignoreBlocks: bool
   let nodes: PatternNode[] = [];
   let cursor = samples[0].walk();
   for (let i = 1; i < samples.length; i++) {
-    [nodes, variablePaths] = compareNodes(cursor, samples[i].walk(), language, ignoreBlocks);
+    const nodeComparison = new NodeComparison(cursor, samples[i].walk(), language);
+    [nodes, variablePaths] = nodeComparison.execute(ignoreBlocks);
     cursor = new PatternCursor(nodes[0]);
   }
   return { pattern: nodes[0], variablePaths };
 }
 
-function compareNodes(
-  a: TreeCursor,
-  b: TreeCursor,
-  language: Language,
-  ignoreBlocks: boolean,
-  path: Path = []
-): [PatternNode[], Path[]] | null {
-  const blockNodeType = loadBlocksConfigFor(language).blockNodeType;
-  const nodes: PatternNode[] = [];
-  let variablePaths: Path[] = [];
+class NodeComparison {
+  // Input
+  private ignoreBlocks = false;
+  private blockNodeType: string;
 
-  let hasSibling = true;
-  for (let index = 0; hasSibling; index++) {
-    const fieldNameA = a.currentFieldName() || undefined;
-    const fieldNameB = b.currentFieldName() || undefined;
-    if (fieldNameA !== fieldNameB) {
-      // mismatch (parent)
-      return null;
-    }
-    if (a.nodeType !== b.nodeType) {
-      if (!a.nodeIsNamed || !b.nodeIsNamed) {
-        // keywords cannot be variable
+  // State
+  private path: Path;
+
+  // Output
+  private nodes: PatternNode[] = [];
+  private variablePaths: Path[] = [];
+
+  constructor(private a: TreeCursor, private b: TreeCursor, private language: Language) {
+    this.blockNodeType = loadBlocksConfigFor(this.language).blockNodeType;
+  }
+
+  execute(ignoreBlocks: boolean, path: Path = []): [PatternNode[], Path[]] | null {
+    this.ignoreBlocks = ignoreBlocks;
+    this.path = path;
+    let hasSibling = true;
+    for (let index = 0; hasSibling; index++) {
+      const fieldNameA = this.a.currentFieldName() || undefined;
+      const fieldNameB = this.b.currentFieldName() || undefined;
+      if (fieldNameA !== fieldNameB) {
         // mismatch (parent)
         return null;
       }
-      // mismatch (current, wildcard)
-      variablePaths.push(path.concat(index));
-      nodes.push({
-        variable: true,
-        type: "*",
-        fieldName: fieldNameA,
-      });
-    } else if (!ignoreBlocks && a.nodeType === blockNodeType && b.nodeType === blockNodeType) {
-      variablePaths.push(path.concat(index));
-      nodes.push({
-        variable: true,
-        fieldName: fieldNameA,
-        type: a.nodeType,
-      });
-    } else {
-      const hasChildrenA = gotoFirstChild(a);
-      const hasChildrenB = gotoFirstChild(b);
-      if (hasChildrenA !== hasChildrenB) {
-        // mismatch (current, same node type)
-        if (hasChildrenA) {
-          a.gotoParent();
+      if (this.a.nodeType !== this.b.nodeType) {
+        if (!this.a.nodeIsNamed || !this.b.nodeIsNamed) {
+          // keywords cannot be variable
+          // mismatch (parent)
+          return null;
         }
-        if (hasChildrenB) {
-          b.gotoParent();
-        }
-        variablePaths.push(path.concat(index));
-        nodes.push({
+        // mismatch (current, wildcard)
+        this.variablePaths.push(this.path.concat(index));
+        this.nodes.push({
           variable: true,
+          type: "*",
           fieldName: fieldNameA,
-          type: a.nodeType,
         });
-      } else if (hasChildrenA && hasChildrenB) {
-        const result = compareNodes(a, b, language, ignoreBlocks, path.concat(index));
-        a.gotoParent();
-        b.gotoParent();
-        if (result) {
-          const [children, childVariables] = result;
-          variablePaths = variablePaths.concat(childVariables);
-          nodes.push({
-            fieldName: fieldNameA,
-            type: a.nodeType,
-            children,
-          });
-        } else {
-          // mismatch (current, same node type)
-          variablePaths.push(path.concat(index));
-          nodes.push({
-            variable: true,
-            fieldName: fieldNameA,
-            type: a.nodeType,
-          });
-        }
-      } else if (a.nodeText !== b.nodeText) {
-        // mismatch (current, same node type)
-        variablePaths.push(path.concat(index));
-        nodes.push({
+      } else if (
+        !this.ignoreBlocks &&
+        this.a.nodeType === this.blockNodeType &&
+        this.b.nodeType === this.blockNodeType
+      ) {
+        this.variablePaths.push(this.path.concat(index));
+        this.nodes.push({
           variable: true,
           fieldName: fieldNameA,
-          type: a.nodeType,
+          type: this.a.nodeType,
         });
       } else {
-        nodes.push({
-          fieldName: fieldNameA,
-          type: a.nodeType,
-          text: a.nodeText,
-        });
+        const hasChildrenA = gotoFirstChild(this.a);
+        const hasChildrenB = gotoFirstChild(this.b);
+        if (hasChildrenA !== hasChildrenB) {
+          // mismatch (current, same node type)
+          if (hasChildrenA) {
+            this.a.gotoParent();
+          }
+          if (hasChildrenB) {
+            this.b.gotoParent();
+          }
+          this.variablePaths.push(this.path.concat(index));
+          this.nodes.push({
+            variable: true,
+            fieldName: fieldNameA,
+            type: this.a.nodeType,
+          });
+        } else if (hasChildrenA && hasChildrenB) {
+          const childNodeComparison = new NodeComparison(this.a, this.b, this.language);
+          const result = childNodeComparison.execute(this.ignoreBlocks, this.path.concat(index));
+          this.a.gotoParent();
+          this.b.gotoParent();
+          if (result) {
+            const [children, childVariables] = result;
+            this.variablePaths = this.variablePaths.concat(childVariables);
+            this.nodes.push({
+              fieldName: fieldNameA,
+              type: this.a.nodeType,
+              children,
+            });
+          } else {
+            // mismatch (current, same node type)
+            this.variablePaths.push(this.path.concat(index));
+            this.nodes.push({
+              variable: true,
+              fieldName: fieldNameA,
+              type: this.a.nodeType,
+            });
+          }
+        } else if (this.a.nodeText !== this.b.nodeText) {
+          // mismatch (current, same node type)
+          this.variablePaths.push(this.path.concat(index));
+          this.nodes.push({
+            variable: true,
+            fieldName: fieldNameA,
+            type: this.a.nodeType,
+          });
+        } else {
+          this.nodes.push({
+            fieldName: fieldNameA,
+            type: this.a.nodeType,
+            text: this.a.nodeText,
+          });
+        }
       }
-    }
 
-    const hasSiblingA = a.gotoNextSibling();
-    const hasSiblingB = b.gotoNextSibling();
-    if (hasSiblingA !== hasSiblingB) {
-      // mismatch (parent)
-      return null;
+      const hasSiblingA = this.a.gotoNextSibling();
+      const hasSiblingB = this.b.gotoNextSibling();
+      if (hasSiblingA !== hasSiblingB) {
+        // mismatch (parent)
+        return null;
+      }
+      hasSibling = hasSiblingA && hasSiblingB;
     }
-    hasSibling = hasSiblingA && hasSiblingB;
+    return [this.nodes, this.variablePaths];
   }
-  return [nodes, variablePaths];
 }
 
 function gotoFirstChild(cursor: TreeCursor): boolean {
